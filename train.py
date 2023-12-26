42af1eeb1bbbbe30dd3b6a71a8f721fba766ecd9
medicine-wave <1138169474@qq.com>
2023-12-26 06:00:08 +0000

    update train.sh

---
+++ train.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from onmt.bin.train import main
+
+if __name__ == "__main__":
+    main()
---
+++ train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+python train.py -config config/50k_e_smiles_finetune_config.yml
